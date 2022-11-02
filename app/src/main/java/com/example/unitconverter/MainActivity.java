1b63f4e3f7335b212bad46773d3f0d2c1e1cf485
Little Jacob <joelnoumbi28@gmail.com>
2022-11-02 01:36:46 +0100

    conversion is up to be used

--- app/src/main/java/com/example/unitconverter/MainActivity.java
+++ app/src/main/java/com/example/unitconverter/MainActivity.java
@@ -3,12 +3,39 @@ package com.example.unitconverter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView result_text;
+    Button btn;
+    EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        result_text = findViewById(R.id.result_textView);
+        btn = findViewById(R.id.btn);
+        editText = findViewById(R.id.editText);
+
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                double kilos_entered = Double.parseDouble(editText.getText().toString());
+                result_text.setText(""+convertToPounds(kilos_entered));
+            }
+        });
     }
+
+    public double convertToPounds(double kilos){
+        return kilos * 2.20462;
+    }
+
+
 }
